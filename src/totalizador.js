109032e803da2e781daad5da384b7a37e37f32b9
KathM00 <kathymontamejia@gmail.com>
2026-09-07 22:46:47 -0400

    feat: implementar calculo de impuesto especifico para estado TX

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,3 +1,10 @@
 export function calcularPrecioNeto(cantidad, precio) {
   return cantidad * precio;
 }
+
+export function calcularImpuesto(monto, estado) {
+  if (estado === 'TX') {
+    return monto * 0.0625;
+  }
+  return 0;
+}
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -1,7 +1,12 @@
-import { calcularPrecioNeto } from './totalizador.js';
+import {calcularPrecioNeto, calcularImpuesto} from './totalizador.js';
 
 describe('Calculadora de Totalizador', () => {
   it('debería calcular el precio neto multiplicando cantidad por precio', () => {
     expect(calcularPrecioNeto(20, 3)).toEqual(60);
   });
+
+  it('debería calcular el impuesto del 6.25% para el estado TX', () => {
+    expect(calcularImpuesto(60, 'TX')).toEqual(3.75);
+  });
+
 });
